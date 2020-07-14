e01554eb6868592511737b3dce65750fd6b5d641
zhangchunmeng <zhangchunmeng@kuaishou.com>
2020-07-14 09:37:53 +0800

    多语言

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,8 @@ module.exports = {
       // { name: 'viewport', content: 'width=device-width,initial-scale=0.3,minimum-scale=0.3,maximum-scale=0.3,user-scalable=no' },
       { hid: 'description', name: 'description', content: 'High-quality semiconductor products provider' },
       {name: 'baidu-site-verification', content: "JOVc29aSEV"},
+      {name: 'baidu-site-verification', content: "5E485270F10D6085B8E35ADC3B12C802"},
+      {name: 'msvalidate.01', content: "5E485270F10D6085B8E35ADC3B12C802"},
       {name: 'google-site-verification', content: "OrJbGio1EtsulKr1cEXtPe4nKVxkhg7Vj3S4qF3jaqo"},
       {name: 'keywords', content: 'Semiconductor silicon wafer,1inch silicon wafer,2inch silicon wafer,3inch silicon wafer,4inch silicon wafer,5inch silicon wafer,6inch silicon wafer,8inch silicon wafer,12inch silicon wafer,FZ wafer,CZ wafer,SiC wafer,dicing wafer,grinding wafer,oxide film wafer,Silicon nitride,solar cell,solar solar panel,used solar panel,solar energy,silicon nitride thermal plate,silicon nitride powder,silicon nitride device,Si3N4,metal film'}
     ],
